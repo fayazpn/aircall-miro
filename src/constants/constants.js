8396a1fae92eac1d1c44f079d538591cee7dc96f
Fayaz <pnfayaz@gmail.com>
2024-01-05 15:14:59 +0400

    feat(header): routing in header

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+export const ROUTE_CONSTANTS = {
+  DEFAULT: '/',
+  ALL_CALLS: '/all-calls',
+  CALL_DETAILS: '/call-details/:callerId',
+  ARCHIVED_CALLS: '/archived-calls',
+};
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import AllCalls from './pages/AllCalls.jsx';
 import NotFound from './pages/NotFound.jsx';
+import { ROUTE_CONSTANTS } from './constants/constants.js';
 
 // Lazy loading pages
 const CallDetail = React.lazy(() => import('./pages/CallDetail.jsx'));
@@ -11,19 +12,19 @@ const ArchivedCalls = React.lazy(() =>
 
 export const publicRoutes = [
   {
-    path: '/',
+    path: ROUTE_CONSTANTS.DEFAULT,
     element: React.createElement(AllCalls),
   },
   {
-    path: '/all-calls',
+    path: ROUTE_CONSTANTS.ALL_CALLS,
     element: React.createElement(AllCalls),
   },
   {
-    path: '/call/:callId',
+    path: ROUTE_CONSTANTS.CALL_DETAILS,
     element: React.createElement(CallDetail),
   },
   {
-    path: '/archived-calls',
+    path: ROUTE_CONSTANTS.ARCHIVED_CALLS,
     element: React.createElement(ArchivedCalls),
   },
   {
